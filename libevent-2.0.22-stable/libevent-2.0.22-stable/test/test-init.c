710769f7156e400bb5c1ee2f69453508231a74e5
adofsauron <adofsauron@163.com>
2016-01-18 22:39:46 +0800

    fix test-init

--- libevent-2.0.22-stable/libevent-2.0.22-stable/test/test-init.c
+++ libevent-2.0.22-stable/libevent-2.0.22-stable/test/test-init.c
@@ -1,36 +1,15 @@
 /*
- * Copyright (c) 2003-2007 Niels Provos <provos@citi.umich.edu>
- * Copyright (c) 2007-2012 Niels Provos and Nick Mathewson
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Compile with:
+ * cc -I/usr/local/include -o time-test time-test.c -L/usr/local/lib -levent
  */
-#include "event2/event-config.h"
+#ifdef HAVE_CONFIG_H
+//#include "event-config.h"
+#endif
+
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#ifdef _EVENT_HAVE_SYS_TIME_H
 #include <sys/time.h>
-#endif
 #ifdef _EVENT_HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
@@ -38,27 +17,80 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#ifndef WIN32
 #include <unistd.h>
-#endif
 #include <errno.h>
 
 #include <event.h>
 
+ static void
+write_cb(int fd, short event, void *arg)
+{
+	const char *test = "test call back";
+	int len;
+
+	len = write(fd, test, strlen(test) + 1);
+
+	printf("%s %s %d: write %d -> %s\n",__FILE__, __func__,__LINE__,
+		 len, test);
+
+}
+
+ static void
+read_cb(int fd, short event, void *arg)
+{
+	const int length = 256;
+	char test[length] ;
+	int len;
+
+	len = read(fd, test, length);
+
+	printf("%s %s %d: read %d -> %s\n",__FILE__, __func__,__LINE__,
+		 len, test);
+
+}
+
+ static void
+ add_event(int fd, short EV_FLAGS, void* (*callback) (int , short , void *))
+ {
+ 	struct event ev;
+ 	event_set(&ev, fd, EV_FLAGS, callback, &ev);
+	event_add(&ev, NULL);
+ }
+
 int
 main(int argc, char **argv)
 {
-#ifdef WIN32
-	WORD wVersionRequested;
-	WSADATA wsaData;
+	struct event ev0, ev1;
+	int pair[2];
 
-	wVersionRequested = MAKEWORD(2, 2);
-
-	(void) WSAStartup(wVersionRequested, &wsaData);
-#endif
+	// if (evutil_socketpair(AF_UNIX, SOCK_STREAM, 0, pair) == -1)
+	// 	return (1);
 
 	/* Initalize the event library */
-	event_init();
+	struct event_base *base = event_init();
+
+	
+	if (pipe(pair) == -1) 
+	{
+		if (evutil_socketpair(AF_UNIX, SOCK_STREAM, 0, pair) == -1)
+		 {
+			perror("pipe");
+			exit(1);		
+		}
+	}
+
+	event_set(&ev0, pair[0], EV_READ, read_cb, &ev0);
+	event_set(&ev1, pair[1	], EV_WRITE, write_cb, &ev1);
+
+	event_add(&ev0, NULL);
+	event_add(&ev1, NULL);
+
+	// add_event(pair[1], EV_WRITE, write_cb);
+
+	// add_event(pair[0], EV_READ, read_cb);
+
+
+	event_base_dispatch(base);
 
 	return (0);
 }
